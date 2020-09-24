ef4e3b6d3f723f16c1e4216b3eee0579a52dc157
azezmohammed <muhamadaziz047@gmail.com>
2020-09-24 20:01:42 +0700

    yarn remove mbreact and ant design

--- src/index.js
+++ src/index.js
@@ -2,14 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-// Mdbreact
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "bootstrap-css-only/css/bootstrap.min.css";
-import "mdbreact/dist/css/mdb.css";
-// AntDesign
-import "antd/dist/antd.css";
-// React Bootstrap
-import "bootstrap/dist/css/bootstrap.min.css";
+
 // Bungkus App js dengan Provider
 import { Provider } from "react-redux";
 // Import Store
