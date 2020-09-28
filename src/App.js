abafe9159ccc73123e411f49e942be89ef9f948b
azezmohammed <muhamadaziz047@gmail.com>
2020-09-28 22:50:54 +0700

    add some component and react router dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from "react";
 import "./App.css";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 // Import Components
 import Header from "./Components/Header";
+import MovieNowPlaying from "./Components/MovieNowPlaying";
+import Upcoming from "./Components/Upcoming";
+import Home from "./Components/Home";
 
 function App() {
   return (
-    <div>
-      <Header />
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route>
+            <Header />
+          </Route>
+          <Route>
+            <Home />
+          </Route>
+          <Route>
+            <MovieNowPlaying />
+          </Route>
+          <Route>
+            <Upcoming />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/Upcoming.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Upcoming() {
+  return (
+    <div>
+      <h1>Ini halaman Upcoming </h1>
+    </div>
+  );
+}
+
+export default Upcoming;
