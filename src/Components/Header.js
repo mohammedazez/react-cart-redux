8a05c0474a03d77f3d0941583d46b4d8ad4856b7
azezmohammed <muhamadaziz047@gmail.com>
2020-10-01 11:41:02 +0700

    add styling in header or navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,60 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarNav,
+  MDBNavItem,
+  MDBNavLink,
+  MDBNavbarToggler,
+  MDBCollapse,
+  MDBFormInline,
+} from "mdbreact";
+// import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="app">
-      <ul>
+      <MDBNavbar color="indigo" dark expand="md">
+        <MDBNavbarBrand>
+          <img
+            src={require("../Assets/warflix.png")}
+            style={{ height: "50px", width: "50px" }}
+          />
+          <strong className="white-text">Warflix</strong>
+        </MDBNavbarBrand>
+        <MDBNavbarToggler />
+        <MDBCollapse navbar>
+          <MDBNavbarNav left>
+            <MDBNavItem active>
+              <MDBNavLink to="/">Home</MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink to="/upcoming">Upcoming</MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink to="#!">Now Playing</MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink to="/detail">Detail Movie</MDBNavLink>
+            </MDBNavItem>
+          </MDBNavbarNav>
+          <MDBNavbarNav right>
+            <MDBNavItem>
+              <MDBFormInline waves>
+                <div className="md-form my-0">
+                  <input
+                    className="form-control mr-sm-2"
+                    type="text"
+                    placeholder="Search"
+                    aria-label="Search"
+                  />
+                </div>
+              </MDBFormInline>
+            </MDBNavItem>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBNavbar>
+      {/* <ul>
         <li>
           <Link to="/">Home</Link>
         </li>
@@ -14,7 +64,7 @@ function Header() {
         <li>
           <Link to="/detail">Detail Movie</Link>
         </li>
-      </ul>
+      </ul> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+// Import Mdbreact
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
 import App from "./App";
 // Bungkus App js dengan Provider
 import { Provider } from "react-redux";
