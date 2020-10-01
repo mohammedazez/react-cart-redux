2144b7c097d9d76752086ae1da24173d620ba5ce
azezmohammed <muhamadaziz047@gmail.com>
2020-10-01 14:42:17 +0700

    Home styling

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,18 @@
 import React from "react";
+import "../Style/Home.css";
 
 function Home() {
   return (
-    <div>
-      <h1>Ini halaman Home</h1>
+    <div className="wrapper">
+      <div className="container-text">
+        <p className="text">W</p>
+        <p className="text">A</p>
+        <p className="text">R</p>
+        <p className="text">F</p>
+        <p className="text">L</p>
+        <p className="text">I</p>
+        <p className="text">X</p>
+      </div>
     </div>
   );
 }
